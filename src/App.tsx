86813167bbf55b81283693c4ccc7d947d79db507
Anas <laangryanas@gmail.com>
2023-11-08 19:48:55 +0100

    feat: added new elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,88 @@
 import "./App.css";
 
+import { useState } from "react";
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { 
+  getFirestore,
+  collection,
+  query,
+  limit,
+  orderBy
+} from "firebase/firestore";
 
 // Initialize Firebase
 const app = initializeApp(JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG));
-
 const auth = getAuth();
+const db = getFirestore(app)
+
+type User = {
+  uid: String;
+  photoURL: String;
+  displayName: String;
+}
 
 function App() {
+  const [user] = useAuthState(auth)
+  const [newPost, useNewPost] = useState(false)
+
+ async function submit(e:React.FormEvent) {
+  e.preventDefault()
+ // const { uid, photoURL, displayName } = auth.currentUser as User;
+ // if (formValue != undefined)
+ console.log(e)
+ }
+  
   return (
     <>
+    <header>
       <h1>Cosmo</h1>
+    </header>
+    <div>{user ? (
+    <div className="welcome">
       <h3>Welcome, user</h3>
+      <SignOut/></div>
+      ) : <SignIn/> }</div>
+      <form className="post" onSubmit={submit}>
+        <button 
+        onClick={()=>useNewPost(prev=>!prev)}
+        type="button"
+        >New Post</button>
+        {newPost && <NewPost/>}
+        <button type='submit'>Submit</button>
+      </form>
+    <Gallery/>
     </>
   );
+}
+
+function NewPost() {
+  return (
+  <>
+  <input type="file" accept="image/*"></input>
+  </>
+  )
+}
+
+function Gallery() {
+  
+  const galleryRef = collection(db, "posts")
+  const q = query(galleryRef, orderBy("createdAt"), limit(3))
+  return(
+    <>
+    <h1>All Posts</h1>
+    </>
+  )
+}
+// From post collection on firestore
+function Post() {
+  return(<>
+  <div className="post">
+    <p>Author</p>
+    <p>Image</p>
+  </div>
+    </>)
 }
 
 function SignIn() {
