fdb76581002a7b71433cb44e3a354d6a9cb69df1
Apix Nova <laangryanas@gmail.com>
2023-11-12 15:27:41 +0100

    feat: user profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import "./App.css";
 
-import { ChangeEvent, ChangeEventHandler, useState } from "react";
+import { useState } from "react";
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
 import {
   getFirestore,
   collection,
   query,
   limit,
-  orderBy,
   addDoc,
   serverTimestamp,
+  where,
 } from "firebase/firestore";
 import { v4 } from "uuid";
 
@@ -25,6 +26,7 @@ const auth = getAuth();
 const db = getFirestore(app);
 //Create root reference
 const storage = getStorage();
+const postsRef = collection(db, "posts");
 
 type User = {
   uid: String;
@@ -32,16 +34,20 @@ type User = {
   displayName: String;
 };
 
+type Uid = {
+  uid: String | undefined;
+};
+
 function App() {
   const [user] = useAuthState(auth);
   const [newPost, setNewPost] = useState(false);
   const [image, setImage] = useState<File | null>(null);
-
-  const postsRef = collection(db, "posts");
+  const [display, setDisplay] = useState("home");
+  const [profileId, setProfileId] = useState<String>("");
 
   async function submit(e: React.FormEvent) {
     e.preventDefault();
-    if (image != null) {
+    if (image) {
       const storageRef = ref(storage, `posts/${image?.name + v4()}`);
       //upload file
       uploadBytes(storageRef, image as File).then(() => {
@@ -64,10 +70,15 @@ function App() {
     }
   }
 
+  function showProfile(user: String) {
+    setProfileId(user);
+    setDisplay("profile");
+  }
+
   return (
     <>
       <header>
-        <h1>Cosmo</h1>
+        <h1 onClick={() => setDisplay("home")}>Cosmo</h1>
       </header>
       <div>
         {user ? (
@@ -79,44 +90,85 @@ function App() {
           <SignIn />
         )}
       </div>
-      <form className="post" onSubmit={submit}>
-        <button onClick={() => setNewPost((prev) => !prev)} type="button">
-          New Post
-        </button>
-        {newPost && (
-          <input
-            type="file"
-            accept="image/*"
-            onChange={(e: any) => {
-              setImage(e.target.files[0]);
-              console.log(e.target.files[0]);
-            }}
-          ></input>
-        )}
-        <button type="submit">Submit</button>
-      </form>
-      <Gallery />
+      {display == "home" && (
+        <>
+          <form className="new-post" onSubmit={submit}>
+            <button onClick={() => setNewPost((prev) => !prev)} type="button">
+              New Post
+            </button>
+            {newPost && (
+              <input
+                type="file"
+                accept="image/*"
+                onChange={(e: any) => {
+                  setImage(e.target.files[0]);
+                }}
+              ></input>
+            )}
+            <button type="submit">Submit</button>
+          </form>
+          <Gallery showProfile={showProfile} />
+        </>
+      )}
+      {display == "profile" && user && (
+        <>
+          <Profile uid={profileId} showProfile={showProfile} />
+        </>
+      )}
     </>
   );
 }
 
-function Gallery() {
-  const galleryRef = collection(db, "posts");
-  const q = query(galleryRef, orderBy("createdAt"), limit(3));
+function Gallery({ showProfile }: any) {
+  const q = query(postsRef, limit(3));
+  const [posts, loading, error] = useCollection(q);
+
   return (
     <>
       <h1>All Posts</h1>
+      {error && <h2>Error: {JSON.stringify(error)}</h2>}
+      {loading && <h2>Loading...</h2>}
+      <div className="gallery">
+        {posts &&
+          posts.docs.map((post) => (
+            <Post key={post.id} post={post.data()} showProfile={showProfile} />
+          ))}
+      </div>
     </>
   );
 }
 // From post collection on firestore
-function Post() {
+function Post({ post, showProfile }: any) {
   return (
     <>
       <div className="post">
-        <p>Author</p>
-        <p>Image</p>
+        <div className="author">
+          <img src={post.photoURL} className="profile-pic"></img>
+          <p className="username" onClick={() => showProfile(post.uid)}>
+            {post.displayName}
+          </p>
+        </div>
+        <img className="post-image" src={post.postUrl}></img>
       </div>
+    </>
+  );
+}
+
+function Profile({ uid, showProfile }: any) {
+  //make a query for all posts
+  const q = query(postsRef, where("uid", "==", uid), limit(10));
+  const [posts, loading, error] = useCollection(q);
+
+  return (
+    <>
+      <h1>User Profile</h1>
+      <p>{uid}</p>
+      {error && <h2>Error: {JSON.stringify(error)}</h2>}
+      {loading && <h2>Loading...</h2>}
+      {posts &&
+        posts.docs.map((post) => (
+          <Post key={post.id} post={post.data()} showProfile={showProfile} />
+        ))}
     </>
   );
 }
